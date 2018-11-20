7ff71046764e9a8c44e9c8514d0d6c5fbbb13367
felipeescorel <felipe.escorel@gmail.com>
2018-11-19 23:41:58 -0300

    Novo Usuário Fragmento Perfil Fragmento Lista de Jogos OBS: Rever Warnings

---
+++ app/src/main/java/ufrpe/mobile/ifut/NovoUsuario.kt
@@ -0,0 +1,12 @@
+package ufrpe.mobile.ifut
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NovoUsuario : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_novo_usuario)
+    }
+}
